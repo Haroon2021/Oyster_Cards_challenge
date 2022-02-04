7011956787612445df92aeedea56d52650c82fb2
Jack <winterschladen.j@gmail.com>
2022-02-04 16:36:05 +0000

    Step 12 complete: list of journeys added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,50 +1,49 @@
 class Oystercard
 
-  attr_reader :balance, :start_station, :list_of_journeys
+  attr_reader :balance, :entry_station, :list_of_journeys
 
   
   LIMIT = 90
   MIN_BALANCE = 1
   def initialize
     @balance = 0
-    @limit = 90
     #@in_journey = false
     #@min_balance = 1
-    @start_station = nil
+    @entry_station = nil
     @list_of_journeys = []
   end
 
   def top_up(amount)
-    raise "this has exceeded the top up limit value of #{@limit}" if over_limit?(@balance, amount)
+    raise "this has exceeded the top up limit value of #{LIMIT}" if over_limit?(@balance, amount)
     @balance += amount
     @balance
   end
 
-  def touchin(station)
+  def touchin(entry_station)
     raise "can not touch in balance below #{MIN_BALANCE}" if @balance < MIN_BALANCE
     #@in_journey = true
-    @start_station = station
-    
+    @entry_station = entry_station
   end
   
   
-  def touchout(station)
+  def touchout(exit_station)
     #@in_journey = false
     deduct(1)
+    save_journey(exit_station)
     @balance
-    @start_station = nil 
+    @entry_station = nil 
   end
 
   def in_journey?
-    if @start_station == nil 
+    if @entry_station == nil 
       return false
     else true    
     end
   end
 
-  def start_station
-    @start_station
-  end
+  # def start_station
+  #   @entry_station
+  # end
 
 
 
@@ -52,7 +51,7 @@ class Oystercard
   private
 
   def over_limit?(balance,amount)
-    @balance + amount > @limit
+    @balance + amount > LIMIT
   end
 
   def deduct(amount)
@@ -60,4 +59,8 @@ class Oystercard
     @balance
   end
 
+  def save_journey(exit_station)
+    journey = {start: @entry_station, end: exit_station}
+    @list_of_journeys << journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,13 @@ describe Oystercard do
   let(:entry_station) {double :entry_station}
   let(:exit_station) {double :exit_station}
   # tests if an oyster card responds to balance
+  
+  describe '#list_of_journeys' do 
+    it 'has an empty array of journeys by default' do
+      expect(subject.list_of_journeys).to eq []
+    end
+  end
+  
   describe '#balance' do
     it 'should respond to balance' do
       expect(subject).to respond_to(:balance)
@@ -26,29 +33,32 @@ describe Oystercard do
 
   #Testing the touchin method
   describe "#touchin" do
+    # before do
+    #   subject.top_up(5)
+    # end
       #Testing if the function exists
       it {is_expected.to respond_to(:touchin).with(1).argument}
       #let(:station) {double :station}
       #Testing if we create a oystercard then we touch in and it says that the card is on a journey
       it "should update in_journey to true after touching in" do
         subject.top_up(1)
-        subject.touchin(station)
-        allow(station).to receive(:start_station).and_return(true)
+        subject.touchin(entry_station)
+        allow(entry_station).to receive(:start_station).and_return(true)
         #expect(subject.start_station).to eq true
       end
       
       # it "should stop top_up if we add something that breaks the limit" do
       it "should not touch in if the balance is below the min balance" do
         min = Oystercard::MIN_BALANCE
-        expect {subject.touchin(station)}.to raise_error("can not touch in balance below #{min}")
+        expect {subject.touchin(entry_station)}.to raise_error("can not touch in balance below #{min}")
       end
       
       #it should make the card remember the entry station
       
       it "it should make the card remember the entry station" do
         subject.top_up(5)
-        subject.touchin(station)
-        expect(subject.start_station).to eq station
+        subject.touchin(entry_station)
+        expect(subject.entry_station).to eq entry_station
       end 
 
 
@@ -57,21 +67,22 @@ describe Oystercard do
 
   #Testing the touchout method
   describe "#touchout" do
+
+      let(:journey) { {start: entry_station, end: exit_station} }
       it {is_expected.to respond_to(:touchout).with(1).argument}
 
       #Testing if we create a oystercard then we touch out and it says that the card is on a journey
       it "should update in_journey to true after touching out" do
         subject.top_up(1)
-        subject.touchin(station)
-        subject.touchout(station)
+        subject.touchin(entry_station)
+        subject.touchout(exit_station)
         expect(subject.in_journey?).to eq false
       end
 
       it "should save the journey" do
-        let (:journey) {double :{start_station,:en}}
         subject.top_up(5)
-        subject.touchin(station)
-        subject.touchout(station)
+        subject.touchin(entry_station)
+        subject.touchout(exit_station)
         expect(subject.list_of_journeys).to include journey
       end
 
@@ -79,8 +90,8 @@ describe Oystercard do
 
       it "should have an oyster card that touches out and reduces the balance by the min balance which is assumed to be the fare" do
         subject.top_up(78)
-        subject.touchin(station)
-        expect {subject.touchout(station)}.to change {subject.balance}.by(-1)
+        subject.touchin(entry_station)
+        expect {subject.touchout(exit_station)}.to change {subject.balance}.by(-1)
       end
 
       # #Testing if the start station is nul once card is touched out
@@ -97,8 +108,8 @@ describe Oystercard do
   #Testing the in_journey? method
   describe "#in_journey?" do
     it "should return false" do
-    subject.touchout(station)
-      expect(subject.start_station).to eq nil 
+    subject.touchout(exit_station)
+      expect(subject.entry_station).to eq nil 
     end
   end
 
